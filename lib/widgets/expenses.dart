25359bfa40844f52484241bf32eb63679e5206ad
Ömer Okumuş <omerokumus3@gmail.com>
2023-06-01 08:28:01 +0300

    Expenses Widget

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,40 @@
+import 'package:expense_tracker/widgets/expense_list/expense_list.dart';
+import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("The Chart"),
+          Expanded(child: ExpenseList(expenseList: _registeredExpenses))
+        ],
+      ),
+    );
+  }
+}
